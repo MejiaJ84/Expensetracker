9606d63445138578678ff90d2b640b24a35a3aa3
MejiaJ84 <jtmejia1984@gmail.com>
2022-05-25 21:26:30 -0700

    Added Insert, Update, Delete, and Queries to TransactionDao.kt

--- app/src/main/java/com/example/expensetracker/database/TransactionDao.kt
+++ app/src/main/java/com/example/expensetracker/database/TransactionDao.kt
@@ -1,4 +1,29 @@
 package com.example.expensetracker.database
 
+import androidx.room.*
+import com.example.expensetracker.model.Transaction
+import kotlinx.coroutines.flow.Flow
+
+@Dao // Dao: Data Access Object
 interface TransactionDao {
+
+    // Inserts new transaction
+    @Insert(onConflict = OnConflictStrategy.REPLACE) // replaces any existing data with new data that are duplicates
+    suspend fun insertTransaction(transaction: Transaction)
+
+    // Updates existing transaction
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun updateTransaction(transaction: Transaction)
+
+    // Deletes a transaction
+    @Delete
+    suspend fun deleteTransaction(transaction: Transaction)
+
+    // gets all transactions and stores in a list
+    @Query("SELECT * FROM all_transactions")
+    fun getAllTransactions() : Flow<List<Transaction>>
+
+    // gets a list of all income or expenses
+    @Query("SELECT * FROM all_transactions WHERE transactionType == :transactionType")
+    fun getATransaction(transactionType:String) : Flow<List<Transaction>>
 }
