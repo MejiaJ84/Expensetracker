a8f0af7ce4d598a456e5ed7169d747e8f8ef8c1b
MejiaJ84 <jtmejia1984@gmail.com>
2022-06-12 16:52:09 -0700

    Removed files while rebuilding and simplifying app

--- app/src/main/java/com/example/expensetracker/TransactionDatabase.kt
+++ app/src/main/java/com/example/expensetracker/TransactionDatabase.kt
@@ -1,31 +1,25 @@
-package com.example.expensetracker.database
+package com.example.expensetracker
 
 import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import com.example.expensetracker.model.Transaction
 
 @Database(entities = [Transaction::class], version = 1, exportSchema = false)
-abstract class ExpenseDatabase : RoomDatabase() {
-
+abstract class TransactionDatabase : RoomDatabase(){
     abstract val transactionDao: TransactionDao
-
     companion object {
         @Volatile
-        private var INSTANCE: ExpenseDatabase? = null
-        // Check if there is an instance of the database yet and if not, create one
-        fun getInstance(context: Context): ExpenseDatabase {
+        private var INSTANCE: TransactionDatabase? = null
+        fun getInstance(context: Context): TransactionDatabase {
             synchronized(this) {
                 var instance = INSTANCE
-
                 if (instance == null) {
                     instance = Room.databaseBuilder(
                         context.applicationContext,
-                        ExpenseDatabase::class.java,
+                        TransactionDatabase::class.java,
                         "transaction_database"
-                    )
-                        .build()
+                    ).build()
                     INSTANCE = instance
                 }
                 return instance
